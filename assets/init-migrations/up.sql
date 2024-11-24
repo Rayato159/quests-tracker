d7b31a29b72f3aae8ef77a86f7575ae07db30e66
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-11-25 00:49:37 +0700

    :sparkles: fix migrate scriptt

--- assets/init-migrations/up.sql
+++ assets/init-migrations/up.sql
@@ -20,7 +20,7 @@ CREATE TABLE adventurers (
 
 CREATE TABLE guild_commanders (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP NOT NULL DEFAULT now()
